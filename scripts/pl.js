610e3fd0c0b1df7bf77c17088a41d7802be785ab
Git-mkot <mkotserious@gmail.com>
2025-12-03 16:57:48 +0100

    Hyphens android 2

---
+++ scripts/pl.js
@@ -0,0 +1,11 @@
+Hypher.languages.pl = {
+  leftmin: 2,
+  rightmin: 2,
+  patterns: {
+    2: "aa ee oo au eu iu ia io ii",
+    3: "b1c b1p b1t c1h c1k c1s d1r d1w f1k f1l f1r g1l g1r g1w h1l h1r k1l k1r k1w m1l m1r n1t p1l p1r r1b r1c r1d r1f r1g r1k r1p r1s r1t r1w s1k s1l s1m s1n s1p s1t s1w t1k t1l t1r t1s w1k w1l w1r z1b z1d z1g z1l z1m z1n z1r z1w",
+    4: "1ba 1be 1bi 1bo 1bu 1by 1ca 1ce 1ci 1co 1cu 1cy 1da 1de 1di 1do 1du 1dy 1fa 1fe 1fi 1fo 1fu 1fy 1ga 1ge 1gi 1go 1gu 1gy 1ha 1he 1hi 1ho 1hu 1hy 1ja 1je 1ji 1jo 1ju 1jy 1ka 1ke 1ki 1ko 1ku 1ky 1la 1le 1li 1lo 1lu 1ly 1ma 1me 1mi 1mo 1mu 1my 1na 1ne 1ni 1no 1nu 1ny 1pa 1pe 1pi 1po 1pu 1py 1ra 1re 1ri 1ro 1ru 1ry 1sa 1se 1si 1so 1su 1sy 1ta 1te 1ti 1to 1tu 1ty 1va 1ve 1vi 1vo 1vu 1vy 1wa 1we 1wi 1wo 1wu 1wy 1za 1ze 1zi 1zo 1zu 1zy",
+    5: "2ch2 2cz2 2dz2 2dź2 2dż2 2rz2 2sz2 2ść2 2żd2",
+    6: "3str 3spr 3skl 3słm 3prz 3pst 3wst 3wpr"
+  }
+};
